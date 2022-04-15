729ac2d988f8f3674b6c3d5da5d571a6178b23ea
marcosdenisalves <dee.alves2@gmail.com>
2022-04-15 14:51:35 -0300

    Handling with keyboard

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    keyboard: {
+      resize: 'body',
+      style: 'dark',
+      resizeOnFullScreen: true,
+    },
+  },
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { IonRouterOutlet, Platform } from '@ionic/angular';
+import { Component, Optional } from '@angular/core';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    @Optional() private routerOutlet?: IonRouterOutlet
+    ) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (this.routerOutlet && !this.routerOutlet.canGoBack()) {}
+        App.exitApp();
+    });
+
+    this.platform.keyboardDidShow.subscribe(ev => {
+      const { keyboardHeight } = ev;
+      console.log('showing keyboard', keyboardHeight);
+    });
+
+    this.platform.keyboardDidHide.subscribe(() => {
+      console.log('hidding keyboard');
+    });
+  }
 }
